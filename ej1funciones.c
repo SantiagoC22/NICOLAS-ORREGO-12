14331139f7c50b25132b616a3fe185b74f89cecb
SantiagoC22 <86362788+SantiagoC22@users.noreply.github.com>
2021-09-14 10:25:02 -0300

    CUK

--- ej1funciones.c
+++ ej1funciones.c
@@ -4,13 +4,4 @@ int numeros(int a,int b){
 	printf("\nel segundo valor es: %d",b);
 }
 int main(){
-	int n1,n2;
-	printf("ingrese un numero: ");
-	scanf("%d",&n1);
-	printf("ingrese otro numero: ");
-	scanf("%d",&n2);
-	
-	numeros(n1,n2);
-	
-	
-}
+	int n1,
